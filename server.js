a186d29b6534cd9dbd85268dbe9b7609816fd14e
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-06-24 23:00:39 -0700

    handlebars added to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
